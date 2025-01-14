1a738e6e2d65fe4a509b55da83332098c170cce0
honda <bentianhuangtailang@gmail.com>
2025-01-15 06:14:55 +0900

    test commit

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hello World!')
     }
 
